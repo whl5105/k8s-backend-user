3cc2a9d27c891b995e77eea0cbad511f9b5f3b23
suin <73993670+whl5105@users.noreply.github.com>
2025-06-17 13:23:18 +0900

    test 컨트롤러 추가

--- src/main/java/com/welab/k8s_backend_user/api/open/UserAuthController.java
+++ src/main/java/com/welab/k8s_backend_user/api/open/UserAuthController.java
@@ -9,10 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -31,5 +28,10 @@ public class UserAuthController {
     public ApiResponseDto<TokenDto.AccessRefreshToken> login(@RequestBody @Valid SiteUserLoginDto loginDto) {
         TokenDto.AccessRefreshToken token = siteUserService.login(loginDto);
         return  ApiResponseDto.createOk(token);
+    }
+
+    @GetMapping(value = "/test")
+    public ApiResponseDto<String> test() {
+        return ApiResponseDto.createOk("버전 1입니다.");
     }
 }
